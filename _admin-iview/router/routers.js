5a20369acd761e4613e1de420bdad48f773020a1
luo0412 <1095847440@qq.com>
2019-11-16 20:46:00 +0800

    adjust

--- _admin-iview/router/routers.js
+++ _admin-iview/router/routers.js
@@ -25,7 +25,7 @@ export default [
       title: 'Login - 登录',
       hideInMenu: true
     },
-    component: () => import('../view/login/login.vue')
+    component: () => import('../pages/login/login.vue')
   },
   {
     path: '/',
@@ -46,7 +46,7 @@ export default [
           notCache: true,
           icon: 'md-home'
         },
-        component: () => import('../view/single-page/home')
+        component: () => import('../pages/single-page/home')
       }
     ]
   },
@@ -75,12 +75,12 @@ export default [
           icon: 'md-notifications',
           title: '消息中心'
         },
-        component: () => import('../view/single-page/message/index.vue')
+        component: () => import('../pages/single-page/message/index.vue')
       }
     ]
   },
   {
-    path: '/components',
+    path: '/_components',
     name: 'components',
     meta: {
       icon: 'logo-buffer',
@@ -95,7 +95,7 @@ export default [
           icon: 'md-arrow-dropdown-circle',
           title: '树状下拉选择器'
         },
-        component: () => import('../view/components/tree-select/index.vue')
+        component: () => import('../pages/_components/tree-select/index.vue')
       },
       {
         path: 'count_to_page',
@@ -104,7 +104,7 @@ export default [
           icon: 'md-trending-up',
           title: '数字渐变'
         },
-        component: () => import('../view/components/count-to/count-to.vue')
+        component: () => import('../pages/_components/count-to/count-to.vue')
       },
       {
         path: 'drag_list_page',
@@ -113,7 +113,7 @@ export default [
           icon: 'ios-infinite',
           title: '拖拽列表'
         },
-        component: () => import('../view/components/drag-list/drag-list.vue')
+        component: () => import('../pages/_components/drag-list/drag-list.vue')
       },
       {
         path: 'drag_drawer_page',
@@ -122,7 +122,7 @@ export default [
           icon: 'md-list',
           title: '可拖拽抽屉'
         },
-        component: () => import('../view/components/drag-drawer')
+        component: () => import('../pages/_components/drag-drawer')
       },
       {
         path: 'org_tree_page',
@@ -131,7 +131,7 @@ export default [
           icon: 'ios-people',
           title: '组织结构树'
         },
-        component: () => import('../view/components/org-tree')
+        component: () => import('../pages/_components/org-tree')
       },
       {
         path: 'tree_table_page',
@@ -140,7 +140,7 @@ export default [
           icon: 'md-git-branch',
           title: '树状表格'
         },
-        component: () => import('../view/components/tree-table/index.vue')
+        component: () => import('../pages/_components/tree-table/index.vue')
       },
       {
         path: 'cropper_page',
@@ -149,7 +149,7 @@ export default [
           icon: 'md-crop',
           title: '图片裁剪'
         },
-        component: () => import('../view/components/cropper/cropper.vue')
+        component: () => import('../pages/_components/cropper/cropper.vue')
       },
       {
         path: 'tables_page',
@@ -158,7 +158,7 @@ export default [
           icon: 'md-grid',
           title: '多功能表格'
         },
-        component: () => import('../view/components/tables/tables.vue')
+        component: () => import('../pages/_components/tables/tables.vue')
       },
       {
         path: 'split_pane_page',
@@ -167,7 +167,7 @@ export default [
           icon: 'md-pause',
           title: '分割窗口'
         },
-        component: () => import('../view/components/split-pane/split-pane.vue')
+        component: () => import('../pages/_components/split-pane/split-pane.vue')
       },
       {
         path: 'markdown_page',
@@ -176,7 +176,7 @@ export default [
           icon: 'logo-markdown',
           title: 'Markdown编辑器'
         },
-        component: () => import('../view/components/markdown/markdown.vue')
+        component: () => import('../pages/_components/markdown/markdown.vue')
       },
       {
         path: 'editor_page',
@@ -185,7 +185,7 @@ export default [
           icon: 'ios-create',
           title: '富文本编辑器'
         },
-        component: () => import('../view/components/editor/editor.vue')
+        component: () => import('../pages/_components/editor/editor.vue')
       },
       {
         path: 'icons_page',
@@ -194,7 +194,7 @@ export default [
           icon: '_bear',
           title: '自定义图标'
         },
-        component: () => import('../view/components/icons/icons.vue')
+        component: () => import('../pages/_components/icons/icons.vue')
       }
     ]
   },
@@ -214,7 +214,7 @@ export default [
           icon: 'ios-document',
           title: '上传Csv'
         },
-        component: () => import('../view/update/update-table.vue')
+        component: () => import('../pages/update/update-table.vue')
       },
       {
         path: 'update_paste_page',
@@ -223,7 +223,7 @@ export default [
           icon: 'md-clipboard',
           title: '粘贴表格数据'
         },
-        component: () => import('../view/update/update-paste.vue')
+        component: () => import('../pages/update/update-paste.vue')
       }
     ]
   },
@@ -243,7 +243,7 @@ export default [
           icon: 'md-add',
           title: '导入EXCEL'
         },
-        component: () => import('../view/excel/upload-excel.vue')
+        component: () => import('../pages/excel/upload-excel.vue')
       },
       {
         path: 'export-excel',
@@ -252,7 +252,7 @@ export default [
           icon: 'md-download',
           title: '导出EXCEL'
         },
-        component: () => import('../view/excel/export-excel.vue')
+        component: () => import('../pages/excel/export-excel.vue')
       }
     ]
   },
@@ -272,7 +272,7 @@ export default [
           title: '工具方法',
           beforeCloseName: 'before_close_normal'
         },
-        component: () => import('../view/tools-methods/tools-methods.vue')
+        component: () => import('../pages/tools-methods/tools-methods.vue')
       }
     ]
   },
@@ -291,7 +291,7 @@ export default [
           icon: 'md-planet',
           title: 'i18n - {{ i18n_page }}'
         },
-        component: () => import('../view/i18n/i18n-page.vue')
+        component: () => import('../pages/i18n/i18n-page.vue')
       }
     ]
   },
@@ -310,7 +310,7 @@ export default [
           icon: 'ios-bug',
           title: '错误收集'
         },
-        component: () => import('../view/error-store/error-store.vue')
+        component: () => import('../pages/error-store/error-store.vue')
       }
     ]
   },
@@ -330,7 +330,7 @@ export default [
           icon: 'ios-bug',
           title: '错误收集'
         },
-        component: () => import('../view/single-page/error-logger.vue')
+        component: () => import('../pages/single-page/error-logger.vue')
       }
     ]
   },
@@ -349,7 +349,7 @@ export default [
           icon: 'ios-navigate',
           title: '指令'
         },
-        component: () => import('../view/directive/directive.vue')
+        component: () => import('../pages/directive/directive.vue')
       }
     ]
   },
@@ -369,7 +369,7 @@ export default [
           icon: 'md-funnel',
           title: '二级-1'
         },
-        component: () => import('../view/multilevel/level-2-1.vue')
+        component: () => import('../pages/multilevel/level-2-1.vue')
       },
       {
         path: 'level_2_2',
@@ -389,7 +389,7 @@ export default [
               icon: 'md-funnel',
               title: '三级'
             },
-            component: () => import('../view/multilevel/level-2-2/level-2-2-1.vue')
+            component: () => import('../pages/multilevel/level-2-2/level-2-2-1.vue')
           },
           {
             path: 'level_2_2_2',
@@ -398,7 +398,7 @@ export default [
               icon: 'md-funnel',
               title: '三级'
             },
-            component: () => import('../view/multilevel/level-2-2/level-2-2-2.vue')
+            component: () => import('../pages/multilevel/level-2-2/level-2-2-2.vue')
           }
         ]
       },
@@ -409,7 +409,7 @@ export default [
           icon: 'md-funnel',
           title: '二级-3'
         },
-        component: () => import('../view/multilevel/level-2-3.vue')
+        component: () => import('../pages/multilevel/level-2-3.vue')
       }
     ]
   },
@@ -430,7 +430,7 @@ export default [
           notCache: true,
           beforeCloseName: 'before_close_normal'
         },
-        component: () => import('../view/argu-page/params.vue')
+        component: () => import('../pages/argu-page/params.vue')
       },
       {
         path: 'query',
@@ -440,7 +440,7 @@ export default [
           title: route => `{{ query }}-${route.query.id}`,
           notCache: true
         },
-        component: () => import('../view/argu-page/query.vue')
+        component: () => import('../pages/argu-page/query.vue')
       }
     ]
   },
@@ -450,7 +450,7 @@ export default [
     meta: {
       hideInMenu: true
     },
-    component: () => import('../view/error-page/401.vue')
+    component: () => import('../pages/error-page/401.vue')
   },
   {
     path: '/500',
@@ -458,7 +458,7 @@ export default [
     meta: {
       hideInMenu: true
     },
-    component: () => import('../view/error-page/500.vue')
+    component: () => import('../pages/error-page/500.vue')
   },
   {
     path: '*',
@@ -466,6 +466,6 @@ export default [
     meta: {
       hideInMenu: true
     },
-    component: () => import('../view/error-page/404.vue')
+    component: () => import('../pages/error-page/404.vue')
   }
 ]
